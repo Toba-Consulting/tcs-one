170900dd145f7a89c0ad9a2fc26155a91404a744
David <devnull@localhost>
2019-08-21 11:15:52 +0700

    TCS_MRequisition override ReactiveIt(), comment line calling reverseCorrectIt()

--- id.tcs.module/src/org/compiere/model/TCS_MRequisition.java
+++ id.tcs.module/src/org/compiere/model/TCS_MRequisition.java
@@ -1,12 +1,14 @@
 package org.compiere.model;
 
 import java.util.Properties;
+import java.util.logging.Level;
 
 import org.compiere.process.DocAction;
 import org.compiere.process.DocOptions;
 
 public class TCS_MRequisition extends MRequisition implements DocOptions {
 
+	private String			m_processMsg = null;
 	/**
 	 * 
 	 */
@@ -16,6 +18,25 @@ public class TCS_MRequisition extends MRequisition implements DocOptions {
 		super(ctx, M_Requisition_ID, trxName);
 	}
 
+	@Override
+	public boolean reActivateIt() {
+		if (log.isLoggable(Level.INFO)) log.info("reActivateIt - " + toString());
+		// Before reActivate
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REACTIVATE);
+		if (m_processMsg != null)
+			return false;
+
+	//	setProcessed(false);
+//		if (! reverseCorrectIt())
+		//	return false;
+
+		// After reActivate
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REACTIVATE);
+		if (m_processMsg != null)
+			return false;
+
+		return true;
+	}
 	@Override
 	public int customizeValidActions(String docStatus, Object processing, String orderType, String isSOTrx,
 			int AD_Table_ID, String[] docAction, String[] options, int index) {
