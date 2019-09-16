0eeb1cb9317f6170ed28a58b8c59049d5e04f696
David <devnull@localhost>
2019-09-16 17:47:16 +0700

    TCS_OrderLineValidator BeforeDelete Add RemoveMatchPR

--- id.tcs.module/src/id/tcs/validator/TCS_OrderLineValidator.java
+++ id.tcs.module/src/id/tcs/validator/TCS_OrderLineValidator.java
@@ -6,6 +6,7 @@ import org.compiere.model.PO;
 import org.compiere.util.DB;
 import org.osgi.service.event.Event;
 
+import id.tcs.model.X_M_MatchPR;
 import id.tcs.model.X_M_MatchQuotation;
 
 public class TCS_OrderLineValidator {
@@ -15,12 +16,21 @@ public class TCS_OrderLineValidator {
 		
 		if (event.getTopic().equals(IEventTopics.PO_BEFORE_DELETE)) {
 			msg += removeMatchQuotation(orderLine);
+			msg += removeMatchPR(orderLine);
 		}
 		return msg;
 	}
 	
 	private static String removeMatchQuotation(MOrderLine orderLine) {
 		StringBuilder sql = new StringBuilder("DELETE FROM ").append(X_M_MatchQuotation.Table_Name)
+				.append(" WHERE C_OrderLine_ID=?");
+		DB.executeUpdateEx(sql.toString(), new Object[] {orderLine.get_ID()}, orderLine.get_TrxName());
+
+		return "";
+	}
+
+	private static String removeMatchPR(MOrderLine orderLine) {
+		StringBuilder sql = new StringBuilder("DELETE FROM ").append(X_M_MatchPR.Table_Name)
 				.append(" WHERE C_OrderLine_ID=?");
 		DB.executeUpdateEx(sql.toString(), new Object[] {orderLine.get_ID()}, orderLine.get_TrxName());
 
