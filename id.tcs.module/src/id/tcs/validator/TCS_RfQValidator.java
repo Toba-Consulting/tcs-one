ece067e52304d4dc98d9a97ef841ec485161cc1d
David <devnull@localhost>
2019-09-16 17:46:53 +0700

    TCS_RfqValidator Change used mclass

--- id.tcs.module/src/id/tcs/validator/TCS_RfQValidator.java
+++ id.tcs.module/src/id/tcs/validator/TCS_RfQValidator.java
@@ -2,17 +2,18 @@ package id.tcs.validator;
 
 import org.adempiere.base.event.IEventTopics;
 import org.adempiere.exceptions.DBException;
-import org.compiere.model.MRfQ;
 import org.compiere.model.PO;
 import org.compiere.util.DB;
 import org.osgi.service.event.Event;
+
+import id.tcs.model.TCS_MRfQ;
 import id.tcs.model.X_M_MatchInquiry;
 
 public class TCS_RfQValidator {
 
 	public static String executeEvent(Event event, PO po) {
 		String msgRfQ = "";
-		MRfQ rfq = (MRfQ) po;
+		TCS_MRfQ rfq = (TCS_MRfQ) po;
 		 
 		if (event.getTopic().equals(IEventTopics.DOC_AFTER_VOID)) {
 			//msgRfQ = removeMatchRequestRfQ(rfq);
@@ -26,7 +27,7 @@ public class TCS_RfQValidator {
 	 * @param rfq
 	 * @return
 	 */
-	private static String removeMatchInquiry(MRfQ rfq){
+	private static String removeMatchInquiry(TCS_MRfQ rfq){
 		try {
 			StringBuilder sql = new StringBuilder("DELETE FROM ").append(X_M_MatchInquiry.Table_Name).append(" WHERE C_RfQ_ID=?");
 			DB.executeUpdateEx(sql.toString(), new Object[] {rfq.get_ID()}, rfq.get_TrxName());
