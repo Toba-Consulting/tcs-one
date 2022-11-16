6ec4fadc757e66d5303efa7bc24a74b033caac4b
Edwin Ang <edwin.ang.003@gmail.com>
2022-11-16 11:04:18 +0700

    add process to calculate promotion

---
+++ id.tcs.module/src/id/taowi/process/TCS_CalculatePromotion.java
@@ -0,0 +1,113 @@
+package id.taowi.process;
+
+import java.util.logging.Level;
+
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.PromotionRule;
+import org.compiere.model.MOrder;
+import org.compiere.model.MOrderLine;
+import org.compiere.process.ProcessInfoParameter;
+import org.compiere.process.SvrProcess;
+import org.compiere.util.DB;
+
+public class TCS_CalculatePromotion extends SvrProcess {
+
+	private int p_C_Order_ID = 0;
+	@Override
+	protected void prepare() {
+		ProcessInfoParameter[] para = getParameter();
+		for (int i=0; i<para.length; i++) {
+			String name = para[i].getParameterName();
+			if (para[i].getParameter() == null) {
+				;
+			} else {
+				log.log(Level.SEVERE, "Unknown parameter: " + name);
+			}
+			
+		}
+		
+		p_C_Order_ID = getRecord_ID();
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		
+		MOrder order = new MOrder(getCtx(), p_C_Order_ID, get_TrxName());
+		
+		try {
+			PromotionRule.applyPromotions(order);
+			order.getLines(true, null);
+			order.calculateTaxTotal();
+			order.saveEx();
+			increasePromotionCounter(order);
+		} catch (Exception e) {
+			if (e instanceof RuntimeException)
+				throw (RuntimeException)e;
+			else
+				throw new AdempiereException(e.getLocalizedMessage(), e);
+		}
+		
+		
+		return null;
+	}
+	
+	private void increasePromotionCounter(MOrder order) {
+		MOrderLine[] lines = order.getLines(false, null);
+		String promotionCode = (String)order.get_Value("PromotionCode");
+		for (MOrderLine ol : lines) {
+			if (ol.getC_Charge_ID() > 0) {
+				Integer promotionID = (Integer) ol.get_Value("M_Promotion_ID");
+				if (promotionID != null && promotionID.intValue() > 0) {
+
+					int M_PromotionPreCondition_ID = findPromotionPreConditionId(
+							order, promotionCode, promotionID);
+					if (M_PromotionPreCondition_ID > 0) {
+						String update = "UPDATE M_PromotionPreCondition SET PromotionCounter = PromotionCounter + 1 WHERE M_PromotionPreCondition_ID = ?";
+						DB.executeUpdate(update, M_PromotionPreCondition_ID, order.get_TrxName());
+					}
+				}
+			}
+		}
+	}
+	
+	private int findPromotionPreConditionId(MOrder order, String promotionCode,
+			Integer promotionID) {
+		String bpFilter = "M_PromotionPreCondition.C_BPartner_ID = ? OR M_PromotionPreCondition.C_BP_Group_ID = ? OR (M_PromotionPreCondition.C_BPartner_ID IS NULL AND M_PromotionPreCondition.C_BP_Group_ID IS NULL)";
+		String priceListFilter = "M_PromotionPreCondition.M_PriceList_ID IS NULL OR M_PromotionPreCondition.M_PriceList_ID = ?";
+		String warehouseFilter = "M_PromotionPreCondition.M_Warehouse_ID IS NULL OR M_PromotionPreCondition.M_Warehouse_ID = ?";
+		String dateFilter = "M_PromotionPreCondition.StartDate <= ? AND (M_PromotionPreCondition.EndDate >= ? OR M_PromotionPreCondition.EndDate IS NULL)";
+
+		StringBuilder select = new StringBuilder();
+		select.append(" SELECT M_PromotionPreCondition.M_PromotionPreCondition_ID FROM M_PromotionPreCondition ")
+			.append(" WHERE")
+			.append(" (" + bpFilter + ")")
+			.append(" AND (").append(priceListFilter).append(")")
+			.append(" AND (").append(warehouseFilter).append(")")
+			.append(" AND (").append(dateFilter).append(")")
+			.append(" AND (M_PromotionPreCondition.M_Promotion_ID = ?)")
+			.append(" AND (M_PromotionPreCondition.IsActive = 'Y')");
+		if (promotionCode != null && promotionCode.trim().length() > 0) {
+			select.append(" AND (M_PromotionPreCondition.PromotionCode = ?)");
+		} else {
+			select.append(" AND (M_PromotionPreCondition.PromotionCode IS NULL)");
+		}
+		select.append(" ORDER BY M_PromotionPreCondition.C_BPartner_ID Desc, M_PromotionPreCondition.C_BP_Group_ID Desc, M_PromotionPreCondition.M_PriceList_ID Desc, M_PromotionPreCondition.M_Warehouse_ID Desc, M_PromotionPreCondition.StartDate Desc");
+		int M_PromotionPreCondition_ID = 0;
+		int C_BP_Group_ID = 0;
+		try {
+			C_BP_Group_ID = order.getC_BPartner().getC_BP_Group_ID();
+		} catch (Exception e) {
+		}
+		if (promotionCode != null && promotionCode.trim().length() > 0) {
+			M_PromotionPreCondition_ID = DB.getSQLValue(order.get_TrxName(), select.toString(), order.getC_BPartner_ID(),
+					C_BP_Group_ID, order.getM_PriceList_ID(), order.getM_Warehouse_ID(), order.getDateOrdered(),
+					order.getDateOrdered(), promotionID, promotionCode);
+		} else {
+			M_PromotionPreCondition_ID = DB.getSQLValue(order.get_TrxName(), select.toString(), order.getC_BPartner_ID(),
+					C_BP_Group_ID, order.getM_PriceList_ID(), order.getM_Warehouse_ID(), order.getDateOrdered(),
+					order.getDateOrdered(), promotionID);
+		}
+		return M_PromotionPreCondition_ID;
+	}
+
+}
